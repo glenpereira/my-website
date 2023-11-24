8f8b096cdbe2a878a202033baa1a8389afbaa90c
glenpereira <glenv.pereira@gmail.com>
2023-11-24 23:02:24 +0530

    trying to fix dark mode bar theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className='scroll-smooth overflow-y-scroll snap-mandatory scroll-pt-10' lang="en">
+    <Html className='dark scroll-smooth overflow-y-scroll snap-mandatory scroll-pt-10' lang="en">
       <Head />
       <body>
         <Main />
